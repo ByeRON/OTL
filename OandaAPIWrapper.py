0c592c04e2cb57227ac092824f8051faeaef3e4b
byeron <disce.uno@gmail.com>
2020-09-06 16:34:16 +0900

    apiのroot URLを取得する関数を追加

--- OandaAPIWrapper.py
+++ OandaAPIWrapper.py
@@ -5,18 +5,33 @@ import requests
 
 class APIWrapper:
 	def __init__(self, demo=True):
-		with open(sys.argv[1]) as f:
-			auth_tokens = json.load(f)
-			self._id = auth_tokens['oanda_id']
-			self._token = auth_tokens['oanda_token']
-			# debug levelでmsg追加すべき
-		self._api_endpoint = f'https://{}/v3/accounts/{}'
-		self._stream_endpoint = f'https://{}/v3/accounts/{}'
+		API_ROOT = switch_api_root(demo)
+		STREAM_ROOT = switch_stream_root(demo)
+		self._id, self._token = import_auth_info(sys.argv[1])
+		self._api_endpoint = f'https://{API_ROOT}/v3/accounts/{self._id}'
+		self._stream_endpoint = f'https://{STREAM_ROOT}/v3/accounts/{self._id}'
 		self._headers = {
 			'Content-Type': 'application/json',
 			'Authorization': f'Bearer {self._token}'
 		}
 
+	def import_auth_info(self, auth_file):
+		with open(auth_file) as f:
+			auth_info = json.load(f)
+			return (auth_info['oanda_id'], auth_info['oanda_token'])
+	
+	def switch_api_root(self, demo=True):
+		if demo is True:
+			return 'api-fxpractice.oanda.com'
+		else:
+			return 'api-fxtrade.oanda.com'
+
+	def switch_stream_root(self, demo=True):
+		if demo is True:
+			return 'stream-fxpractice.oanda.com'
+		else:
+			return 'stream-fxtrade.oanda.com'
+
 
 if __name__ == '__main__':
-	print('a,')
+	pass
